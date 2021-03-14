a46eb350e18b1543d20d2077b992c9c4c8d544c1
Roksana Dzioba <roksanadzioba@gmail.com>
2021-03-14 21:02:34 +0100

    add commercial projects

--- wp-content/themes/mytheme/page-projects.php
+++ wp-content/themes/mytheme/page-projects.php
@@ -11,27 +11,49 @@
 <section class="all-projects">
     <div class="container">
         <div class="row commercial">
-            <h2>Projekty komerycjne</h2>
+            <div class="section_title"><h2>Projekty komerycjne</h2></div>
             <div class="col-12">
                 <div class="projects__item">
                     <ul class="portfolio_items">
-                        <li class="portfolio_card" id="centrica">
+                        <li class="portfolio_card" id="smart">
                             <figure><img class="img" src="https://via.placeholder.com/544x361.png" alt="I am an example of a work image"></figure>
                             <div class="caption">
-                                <a href="http://www.arthaus.co.uk" target="_blank" title="Centrica" aria-describedby="desc-centrica"><h2>Centrica</h2></a>
-                            <p>Creating an innovative internal business platform</p>
-                                <span class="cta" aria-hidden="true" id="desc-centrica">Explore the work > </span>
+                                <a href="" target="_blank" title="Smart Electric" aria-describedby="desc-smart"><h3>Smart Electric</h3></a>
+                                <p>Strona interentowa dla firmy Smart Electric</p>
+                                <span class="cta" aria-hidden="true" id="desc-smart">Przejdź na stronę internetową</span>
                             </div>
                         </li>
-                        <li class="portfolio_card" id="diageocoupa">
+                        <li class="portfolio_card" id="rehabilitacja">
                             <figure><img class="img" src="https://via.placeholder.com/544x361.png" alt="I am an example of a work image"></figure>
                             <div class="caption">
-                                <a href="http://www.arthaus.co.uk" target="_blank" title="Diageo Coupa" aria-describedby="desc-diageocoupa"><h2>Diageo Coupa</h2></a>
-                            <p>Helping employees boost their skills on the job</p>
-                                <span class="cta" aria-hidden="true" id="desc-diageocoupa">Explore the work > </span>
+                                <a href="" target="_blank" title="Rehabilitacja Covid" aria-describedby="desc-rehabilitacja"><h3>Rehabilitacja Covid</h3></a>
+                                <p>Strona interentowa dla firmy oferującej rehabilitację oddechową</p>
+                                <span class="cta" aria-hidden="true" id="desc-rehabilitacja">Przejdź na stronę internetową</span>
                             </div>
                         </li>
                     </ul>   
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="projects__item">
+                    <ul class="portfolio_items">
+                    <li class="portfolio_card" id="april">
+                            <figure><img class="img" src="https://via.placeholder.com/544x361.png" alt="I am an example of a work image"></figure>
+                            <div class="caption">
+                                <a href="" target="_blank" title="April Development" aria-describedby="desc-april"><h3>April Development</h3></a>
+                                <p>Strona interentowa dla firmy April Development</p>
+                                <span class="cta" aria-hidden="true" id="desc-april">Przejdź na stronę internetową</span>
+                            </div>
+                        </li>
+                        <li class="portfolio_card" id="multidom">
+                            <figure><img class="img" src="https://via.placeholder.com/544x361.png" alt="I am an example of a work image"></figure>
+                            <div class="caption">
+                                <a href="" target="_blank" title="Multidom" aria-describedby="desc-multidom"><h3>Multidom</h3></a>
+                                <p>Strona interentowa dla firmy Multidom</p>
+                                <span class="cta" aria-hidden="true" id="desc-multidom">Przejdź na stronę internetową</span>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
