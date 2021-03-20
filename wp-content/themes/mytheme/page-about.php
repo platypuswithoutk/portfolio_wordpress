ae848cc5b5be9ef4385a1c39d64866b8db058d0d
Roksana Dzioba <roksanadzioba@gmail.com>
2021-03-20 01:17:05 +0100

    add timelines

--- wp-content/themes/mytheme/page-about.php
+++ wp-content/themes/mytheme/page-about.php
@@ -10,11 +10,96 @@
 get_header();
 ?>
 <section id="about">
-    <div class="row">
-      <div class="col-12">
-        <h1><?php the_title(); ?></h1>
-        <div><?php the_content(); ?></div>
+    <div class="row" class="about">
+      <div class="col-12" class="about-desc">
+        <h2>Witam na mojej stronie!</h2>
+        <p></p>
       </div>
+      <div class="col-12" class="photo">
+        <img src="" alt="personal photo">
+      </div>
+    </div>
+
+    <div class="row" class="exp">
+        <div class="col-12">
+            <div class="timeline-area">
+                <div class="timelines">
+                    <h2>Timelines</h2>
+                </div>
+                <div class="all-timelines">
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>April 2019</h6> — The launch of Aryacoin; AYA ICO, resulting in over 30BTC collected
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>December 2019 </h6>— The launch of AYA Pay
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>April 2020</h6> — The successful Hamedan Hardfork, supported by all AYA exchanges, aimed at integrating the dPoW algorithm, improving the security of the AYA blockchain.
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>June 2020</h6> — Aryna Exchange goes live, opening more trading opportunities globally
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>July 2020</h6> — The enabling of our Coin Exchanger
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>November 2020</h6> — The implementation of Smart Contracts into the Aryacoin Ecosystem
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                    <!--SINGLE TIMELINE-->
+                    <div class="single-timeline d-flex-2">
+                        <div class="timeline-blank"></div>
+                        <div class="timeline-text d-flex-2">
+                            <span>
+                                <h6>Q1 2021</h6> — Alef B goes live (more details coming soon)
+                            </span>
+                            <div class="t-square"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 <?php
