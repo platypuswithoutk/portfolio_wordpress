68b03c194c7e35daece75c54e36d3622acf5e078
Roksana Dzioba <roksanadzioba@gmail.com>
2021-03-21 12:10:44 +0100

    add photo to about section

--- wp-content/themes/mytheme/page-about.php
+++ wp-content/themes/mytheme/page-about.php
@@ -17,10 +17,10 @@ get_header();
         <p>Nazywam się Roksana Dzioba i naukę kodowania stron internetowych rozpoczęłam około półtora roku temu.</p>
         <p>Od listopada 2019 roku biorę udział w różnych kursach dażąc do poszerzenia moich kompetencji, poprzez poznawanie najnowszych rozwiązań czy technologii programistycznych. Wzięłam także udział w projekcie typu Open Source.
         <p>Aktualnie zdobywam komercyjne doświadczenie pracując dla firmy Millenium Studio z Krakowa, gdzie zajmuje się kodowaniem stron internetowych oraz sklepów, na podstawie projektu przygotowanego przez grafików.</p>
-        <p>W wolnym czasie, poza programowaniem, interesuje się jeszcze siatkówką, kawą oraz uwielbiam sudoku. </p>
+        <p>W wolnym czasie, poza programowaniem, interesuje się jeszcze siatkówką, kawą oraz uwielbiam sudoku oraz podróże. </p>
       </div>
-      <div class="col-6" class="photo">
-        <img src="" alt="personal photo">
+      <div class="col-6" id="photo">
+	  	<img src="<?php echo get_template_directory_uri(); ?>/assets/img/small_jo.jpg'; ?>" alt="personal-photo">
       </div>
     </div>
 
