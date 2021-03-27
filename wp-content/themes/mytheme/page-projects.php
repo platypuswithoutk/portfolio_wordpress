d79ba00ee5c336a8a815dc13e30ba666eaf92917
Roksana Dzioba <roksanadzioba@gmail.com>
2021-03-27 22:47:07 +0100

    add http adress in commercial projects

--- wp-content/themes/mytheme/page-projects.php
+++ wp-content/themes/mytheme/page-projects.php
@@ -17,20 +17,20 @@
                     <ul class="portfolio_items">
                         <li class="portfolio_card" id="smart">
                             <figure>
-                                <a href=""><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/smart1.png'; ?>" alt="I am an example of a work image"></a>
+                                <a href="https://smartelectric.pl/"><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/smart1.png'; ?>" alt="I am an example of a work image"></a>
                             </figure>
                             <div class="caption">
-                                <a href="" target="_blank" title="Smart Electric" aria-describedby="desc-smart"><h3>Smart Electric</h3></a>
+                                <a href="https://smartelectric.pl/" target="_blank" title="Smart Electric" aria-describedby="desc-smart"><h3>Smart Electric</h3></a>
                                 <p>Strona interentowa dla firmy Smart Electric</p>
                                 <span class="cta" aria-hidden="true" id="desc-smart">Przejdź na stronę internetową</span>
                             </div>
                         </li>
                         <li class="portfolio_card" id="rehabilitacja">
                             <figure>
-                                <a href=""><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/covid(2).png'; ?>" alt="I am an example of a work image"></a>
+                                <a href="rehabilitacja-kr.pl "><img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/covid(2).png'; ?>" alt="I am an example of a work image"></a>
                             </figure>
                             <div class="caption">
-                                <a href="" target="_blank" title="Rehabilitacja Covid" aria-describedby="desc-rehabilitacja"><h3>Rehabilitacja Covid</h3></a>
+                                <a href="rehabilitacja-kr.pl" target="_blank" title="Rehabilitacja Covid" aria-describedby="desc-rehabilitacja"><h3>Rehabilitacja Covid</h3></a>
                                 <p>Strona interentowa dla firmy oferującej rehabilitację oddechową</p>
                                 <span class="cta" aria-hidden="true" id="desc-rehabilitacja">Przejdź na stronę internetową</span>
                             </div>
@@ -43,10 +43,10 @@
                     <ul class="portfolio_items">
                     <li class="portfolio_card" id="april">
                             <figure>
-                                <a href=""><img class="img" src="https://via.placeholder.com/544x361.png" alt="I am an example of a work image"></a>
+                                <a href="http://aprildevelopment.milleniumhost.pl/"><img class="img" src="https://via.placeholder.com/544x361.png" alt="I am an example of a work image"></a>
                             </figure>
                             <div class="caption">
-                                <a href="" target="_blank" title="April Development" aria-describedby="desc-april"><h3>April Development</h3></a>
+                                <a href="http://aprildevelopment.milleniumhost.pl/" target="_blank" title="April Development" aria-describedby="desc-april"><h3>April Development</h3></a>
                                 <p>Strona interentowa dla firmy April Development</p>
                                 <span class="cta" aria-hidden="true" id="desc-april">Przejdź na stronę internetową</span>
                             </div>
