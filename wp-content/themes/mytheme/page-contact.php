6df9fc66ce5483cc9a8f60664cb5bfff26234358
Roksana Dzioba <roksanadzioba@gmail.com>
2021-03-29 22:12:43 +0200

    add img to contact section

--- wp-content/themes/mytheme/page-contact.php
+++ wp-content/themes/mytheme/page-contact.php
@@ -16,6 +16,7 @@ get_header();
     <div class="col-6" id="contact-data">
       <p>Zadzwoń: <a href="tel:">505 015 696</a></p>
       <p>Napisz: <a href="mailto:">roksana.dzioba@gmail.com</a></p>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/girl.png.'; ?>" alt="logo">
     </div>
     <div class="col-6">
       <p class="form-start">Skontaktuj się poprzez formularz:</p>
